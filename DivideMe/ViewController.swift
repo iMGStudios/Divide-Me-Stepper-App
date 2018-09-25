69fea4e83b068df6420cefe6e2ac2d0815e81bbb
Matthew Valentinetti <matthewvalentinetti@medias-iMac-2.local>
2018-09-24 20:36:15 -0500

    Created and hooked up UI

---
+++ DivideMe/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  DivideMe
+//
+//  Created by Matthew Valentinetti on 9/24/18.
+//  Copyright © 2018 Matthew Valentinetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var number1TextField: UITextField!
+    
+    @IBOutlet weak var number2Label: UILabel!
+    
+    @IBOutlet weak var answerLabel1: UILabel!
+    
+    @IBOutlet weak var answerLabel2: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    
+    
+    @IBAction func divideNumbers(_ sender: UIButton) {
+    }
+  
+    
+    
+    
+
+}
+
